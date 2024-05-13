a873187c94eb47d3a088b6ddabed1e30716b67f4
edwardholder <holder.edward@heb.com>
2024-05-13 12:44:50 -0500

    Update issueemail.js

--- incident/issueemail.js
+++ incident/issueemail.js
@@ -2,7 +2,7 @@
 caller_id:d60a489b47060210e3fba3b3416d43a8
 category:software
 subcategory:email
-service:c69b36ac73d423002728660c4cf6a71d
+business_service:c69b36ac73d423002728660c4cf6a71d
 contact_type:email
 */
 const incGr = new GlideRecord('incident');
