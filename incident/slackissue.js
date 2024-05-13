541d5a0b6999dbe2922fbad95fb230c32fe0be5d
edwardholder <holder.edward@heb.com>
2024-05-13 13:02:27 -0500

    Update slackissue.js

--- incident/slackissue.js
+++ incident/slackissue.js
@@ -4,5 +4,6 @@ category:software
 subcategory:email
 business_service:c69b36ac73d423002728660c4cf6a71d
 contact_type:email
+description: cannot see emails
 */
 const incGr = new GlideRecord('incident');
