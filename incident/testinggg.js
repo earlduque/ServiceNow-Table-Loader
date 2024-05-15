8d6179c5c7947ec2db7a19e90b9681809bc1f9d3
edwardholder <holder.edward@heb.com>
2024-05-15 15:28:23 -0500

    Create testinggg.js

---
+++ incident/testinggg.js
@@ -0,0 +1,9 @@
+/*
+caller_id:d60a489b47060210e3fba3b3416d43a8
+category:software
+subcategory:email
+business_service:c69b36ac73d423002728660c4cf6a71d
+contact_type:email
+description: loss of all power
+*/
+const incGr = new GlideRecord('incident');
