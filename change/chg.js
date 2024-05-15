68ae887fa23aa1dffd4d75f8ce2e10b80634930c
edwardholder <holder.edward@heb.com>
2024-05-15 14:52:54 -0500

    Update chg.js

--- change/chg.js
+++ change/chg.js
@@ -1,20 +1,9 @@
-# A sample yaml file
-company: spacelift
-domain:
- - devops
- - devsecops
-tutorial:
-  - yaml:
-      name: "YAML Ain't Markup Language"
-      type: awesome
-      born: 2001
-  - json:
-      name: JavaScript Object Notation
-      type: great
-      born: 2001
-  - xml:
-      name: Extensible Markup Language
-      type: good
-      born: 1996
-author: omkarbirade
-published: true
+/*
+caller_id:d60a489b47060210e3fba3b3416d43a8
+category:devops
+cmdb_ci:33b0c5e047710210e3fba3b3416d43fe
+business_service:c69b36ac73d423002728660c4cf6a71d
+chg_model:devops
+description: testing a github change
+*/
+const incGr = new GlideRecord('change_request');
